c4ccb35e99d16aad8df7a48f181805e6b0af82dd
github username <github email address>
2018-12-05 14:07:49 -0500

    hmmm

---
+++ we-chat-backend/app/channels/group_channel.rb
@@ -0,0 +1,16 @@
+class GroupChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "group_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    if data["message"].length >= 1
+  message = Message.create(content: data["message"])
+    ActionCable.server.broadcast 'group_channel', message.content
+  end
+  end
+end
