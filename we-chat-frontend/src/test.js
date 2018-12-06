951431857ae1f4578219a9e6c4fad6586cc78323
github username <github email address>
2018-12-06 12:26:02 -0500

    rounded

--- we-chat-frontend/src/test.js
+++ we-chat-frontend/src/test.js
@@ -610,7 +610,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 const chatBox = document.querySelector("#chat-box")
 
 do {
-  user = prompt("Enter your username")
+  user = prompt("Enter your username").toLowerCase()
 }
 while (user.length < 2)
 
